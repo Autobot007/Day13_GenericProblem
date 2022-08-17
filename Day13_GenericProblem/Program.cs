f1cc214ae4d57f622503d77a4887c37b7f921dc1
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-08-17 17:49:26 +0530

    [SARFARAAZ]-UC3 Find Maximum Float Number

--- Day13_GenericProblem/Program.cs
+++ Day13_GenericProblem/Program.cs
@@ -8,16 +8,22 @@
             FindMax obj1 = new FindMax();
             while (check)
             {
-                Console.WriteLine("Select option :\n" + "1)Find Integer Maximum Number\n");
+                Console.WriteLine("Select option :\n" + "1)Find Integer Maximum Number\n"+"2)Find Float Maximum Number");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
-                        int result = obj1.FindMaxInteger(4, 3, 2);
+                        int result = obj1.FindMx(4, 3, 2);
                         Console.WriteLine(result);
                         Console.WriteLine("-------------------------------------");
                         Console.WriteLine("Maximum Number is :"+result);
                         break;
+                    case 2:
+                        float resultfloat = obj1.FindMx(4.3f, 3.8f, 2.8f);
+                        Console.WriteLine(resultfloat);
+                        Console.WriteLine("-------------------------------------");
+                        Console.WriteLine("Maximum Number is :" + resultfloat);
+                        break;                        
                     default:
                         check = !check;
                         break;
